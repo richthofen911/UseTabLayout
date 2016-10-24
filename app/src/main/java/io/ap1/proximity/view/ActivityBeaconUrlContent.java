32c997dbcf2ded5f62919dbe083f99fa8435f8e0
richthofen911 <yichaoli.richthofen@gmail.com>
2016-10-24 16:59:22 -0400

    enable javascsript in webview

--- app/src/main/java/io/ap1/proximity/view/ActivityBeaconUrlContent.java
+++ app/src/main/java/io/ap1/proximity/view/ActivityBeaconUrlContent.java
@@ -2,7 +2,9 @@ package io.ap1.proximity.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.suitebuilder.annotation.Suppress;
 import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import io.ap1.proximity.R;
@@ -13,11 +15,15 @@ public class ActivityBeaconUrlContent extends AppCompatActivity {
     WebView wvBeaconUrlContent;
 
     @Override
+    @SuppressWarnings("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon_web_content);
 
         wvBeaconUrlContent = (WebView) findViewById(R.id.wv_beacon_url_content);
+        wvBeaconUrlContent.getSettings().setJavaScriptEnabled(true);
+        wvBeaconUrlContent.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
         String url = getIntent().getStringExtra("url");
         Log.e(TAG, "onCreate: get url: " + url);
         if(url.equals("unknown")) url = "http://www.ap1.io";
